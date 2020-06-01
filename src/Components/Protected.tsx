d8ea0ca84a163ee7894e2635cabd950f52e1e4af
enrico <ezanardo@onezerobinary.com>
2020-06-01 18:09:49 +0200

    Add Auth0 Authentication Service

--- src/Components/Protected.tsx
+++ src/Components/Protected.tsx
@@ -2,9 +2,27 @@ import React from 'react';
 import Auth0 from '../Auth/Auth';
 
 const Protected: React.FC = () => {
+  console.log(Auth0.profile);
+
+  let fullName = Auth0.profile.name;
+  let nickname = Auth0.profile.nickname;
+  let picture = Auth0.profile.picture;
+  let sub = Auth0.profile.sub;
+
   return (
     <div>
-      <p>Hi there!</p>
+      <p>
+        Welcome {fullName} to
+        <h2>ChronoStamp Certification Service</h2>
+      </p>
+
+      <img
+        src={picture}
+        alt={fullName}
+        width="100"
+        className="shadow rounded mb-2"
+      />
+
       <button onClick={() => Auth0.signOut()}>Log Out</button>
     </div>
   );
