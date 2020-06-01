d2197ec230b7eeb954a7a89061a10300269fa532
enrico <ezanardo@onezerobinary.com>
2020-06-01 11:20:58 +0200

    add Auth component

---
+++ src/Auth/Auth.ts
@@ -0,0 +1,84 @@
+import auth0 from 'auth0-js';
+import { ChronoStampAuthConfig, CALLBACK } from './config';
+
+export type AuthConfig = {
+  domain: string;
+  clientID: string;
+  redirectUri: string;
+  responseType: string;
+  scope: string;
+};
+
+class Auth {
+  idToken: string = '';
+  profile: any;
+  expiresAt: number = 0;
+
+  auth0 = new auth0.WebAuth(ChronoStampAuthConfig);
+
+  isAuthenticated(): boolean {
+    return new Date().getTime() > this.expiresAt;
+  }
+
+  signIn() {
+    this.auth0.authorize();
+  }
+
+  async handleAthentication(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.auth0.parseHash((err, authResult) => {
+          if (err) {
+            return reject(err);
+          }
+
+          if (!authResult || !authResult.idToken) {
+            return reject(err);
+          }
+
+          this.setSession(authResult);
+          resolve();
+        });
+      } catch (error) {
+        reject(`It was not possible to handle Athentication. ${error}`);
+      }
+    });
+  }
+
+  setSession(authResult: auth0.Auth0DecodedHash): void {
+    if (authResult.idToken && authResult.idTokenPayload) {
+      this.idToken = authResult.idToken;
+      this.profile = authResult.idTokenPayload;
+      // set the time that the id token will expire at
+      this.expiresAt = authResult.idTokenPayload.expire * 1000;
+    }
+  }
+
+  signOut() {
+    this.auth0.logout({
+      returnTo: CALLBACK,
+      clientID: ChronoStampAuthConfig.clientID,
+    });
+  }
+
+  async silentAuth(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.auth0.checkSession({}, (err, authResult) => {
+          if (err) {
+            return reject(err);
+          }
+
+          this.setSession(authResult);
+          resolve();
+        });
+      } catch (err) {
+        reject(`It was not possible to silentAuth. ${err}`);
+      }
+    });
+  }
+}
+
+const Auth0 = new Auth();
+
+export default Auth0;
