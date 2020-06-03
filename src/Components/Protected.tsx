bbdb2b9f8a98de1c301b83c4bce1c0174ee0548c
enrico <ezanardo@onezerobinary.com>
2020-06-03 15:02:18 +0200

    add a card element to protected page

--- src/Components/Protected.tsx
+++ src/Components/Protected.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Auth0 from '../Auth/Auth';
+
+import { loadStripe } from '@stripe/stripe-js';
+import { STRIPE_PUBLISHABLE_KEY } from '../Auth/config';
+import { Elements, CardElement } from '@stripe/react-stripe-js';
 
 const Protected: React.FC = () => {
   console.log(Auth0.profile);
+
+  const [stripeP, setStripeP] = useState({}) || null;
 
   let fullName = Auth0.profile.name;
   let nickname = Auth0.profile.nickname;
   let picture = Auth0.profile.picture;
   let sub = Auth0.profile.sub;
+
+  const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
+
+  // load the promise
+  useEffect(() => {
+    async function stripe() {
+      let stripe = await loadStripe(STRIPE_PUBLISHABLE_KEY);
+      if (stripe !== null) {
+        setStripeP(stripe);
+
+        console.log(stripeP);
+      }
+    }
+    stripe();
+  }, []);
 
   return (
     <div>
@@ -22,6 +43,10 @@ const Protected: React.FC = () => {
         width="100"
         className="shadow rounded mb-2"
       />
+
+      <Elements stripe={stripePromise}>
+        <CardElement />
+      </Elements>
 
       <button onClick={() => Auth0.signOut()}>Log Out</button>
     </div>
