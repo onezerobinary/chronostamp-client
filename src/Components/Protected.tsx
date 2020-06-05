3866f3fc1319c8b5cd1ccfb6659c5239afafd84a
enrico <ezanardo@onezerobinary.com>
2020-06-05 12:00:19 +0200

    clean unused code

--- src/Components/Protected.tsx
+++ src/Components/Protected.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Auth0 from '../Auth/Auth';
-import { CardElement } from '@stripe/react-stripe-js';
-import axios from 'axios';
 import styled from 'styled-components';
 
-import { BACKEND_URL, API } from '../Auth/config';
 import { Package } from './Package';
 
 interface IAppProps {
@@ -21,54 +18,6 @@ const Protected: React.FC<IAppProps> = (props: IAppProps) => {
   let sub = Auth0.profile.sub;
 
   console.log(sub);
-
-  // load the promise
-  const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const data = new FormData(event.target);
-    const pack = data.get('Pack1');
-
-    console.log(pack);
-
-    // handle payment request to the backend
-    const { stripe, elements } = props;
-    if (!stripe || !elements) {
-      return;
-    }
-
-    const card = elements.getElement(CardElement);
-
-    const result = await stripe.createToken(card);
-    if (result.error) {
-      console.log(result.error.message);
-    } else {
-      console.log(result.token);
-
-      const data = JSON.stringify({
-        token: result.token,
-        amount: pack,
-        chronoStampID: nickname,
-      });
-
-      const URL = `${BACKEND_URL}${API}`;
-
-      let axiosConfig = {
-        mode: 'no-cors',
-        crossorigin: true,
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8',
-          'Access-Control-Allow-Origin': '*',
-        },
-      };
-
-      let response = await axios.post(URL, data, axiosConfig);
-
-      console.log(response);
-    }
-
-    // redirect, clear inputs, thank alert ...
-  };
 
   return (
     <Container>
