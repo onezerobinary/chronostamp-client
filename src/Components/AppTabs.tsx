9b915bbc80244eb27bd194a9592459564d5a78c3
enrico <ezanardo@onezerobinary.com>
2020-06-09 18:50:11 +0200

    keep the background based on the selected tab

--- src/Components/AppTabs.tsx
+++ src/Components/AppTabs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useAppContext } from './AppContextProvider';
 import { ActionType } from '../Actions';
@@ -14,9 +14,13 @@ export const AppTabs: React.FC = () => {
 
   let disabled = state.profile.balance > 0 ? false : true;
 
+  const [tab, setTab] = useState(state.profile.tab);
+
   function handleTab(param: TabsType) {
     let profile = state.profile;
     profile.tab = param;
+
+    setTab(param);
 
     dispatch({
       type: ActionType.setTab,
@@ -26,11 +30,36 @@ export const AppTabs: React.FC = () => {
 
   return (
     <Container>
-      <Button onClick={() => handleTab(TabsType.wallet)}>Wallet</Button>
-      <Button onClick={() => handleTab(TabsType.assets)} disabled={disabled}>
+      <Button
+        style={
+          tab === TabsType.wallet
+            ? { background: '#1c436a', color: '#fff' }
+            : { background: '#009ee3' }
+        }
+        onClick={() => handleTab(TabsType.wallet)}
+      >
+        Wallet
+      </Button>
+      <Button
+        style={
+          tab === TabsType.assets
+            ? { background: '#1c436a', color: '#fff' }
+            : { background: '#009ee3' }
+        }
+        onClick={() => handleTab(TabsType.assets)}
+        disabled={disabled}
+      >
         Assets
       </Button>
-      <Button onClick={() => handleTab(TabsType.search)} disabled={disabled}>
+      <Button
+        style={
+          tab === TabsType.search
+            ? { background: '#1c436a', color: '#fff' }
+            : { background: '#009ee3' }
+        }
+        onClick={() => handleTab(TabsType.search)}
+        disabled={disabled}
+      >
         Search
       </Button>
     </Container>
@@ -50,12 +79,16 @@ const Container = styled.div({
 const Button = styled.button({
   marginLeft: '6px',
   background: '#009ee3',
-  borderRadius: 5,
   border: 'none',
+  borderRadius: 5,
   width: '70px',
   fontSize: '10px',
   ':hover': {
     background: '#1c436a',
     color: '#fff',
+  },
+  ':disabled': {
+    background: '#009ee3',
+    color: 'gray',
   },
 });
